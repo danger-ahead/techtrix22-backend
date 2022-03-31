5196481a9b3fa10310123956d5758bf4c81dd49f
Shourya Shikhar Ghosh <shourya.personal.999@gmail.com>
2022-03-31 11:11:00 +0530

    initialised

---
+++ main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+# from database import engine
+from routes import home
+
+
+# models.Base.metadata.create_all(bind=engine)
+
+
+app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+app.include_router(home.route)
+
+
+@app.get("/")
+def root():
+    return {"message": "Welcome to TechTrix\'22"}
